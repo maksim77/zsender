112736666192b2cf067f01febdfb8313e3810118
Maksim Syomochkin <maksim77ster@gmail.com>
2015-10-19 15:40:13 +0300

    Add quick start sample

--- README.rst
+++ README.rst
@@ -2,6 +2,7 @@ Zabbix Sender
 =============
 Quick Start
 -----------
+::
     from ZabbixSender import ZabbixSender, ZabbixPacket
     server = ZabbixSender('127.0.0.1', 10051)
     packet = ZabbixPacket('myhost','key', 'value')
--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from distutils.core import setup
 
 setup(name='ZabbixSender',
-      version='0.1',
+      version='0.1.1',
       description='Simple zabbix sender',
       author='Maksim Syomochkin',
       author_email='maksim77ster@gmail.com',
       url='https://github.com/maksim77/zsender',
-      py_modules=['ZabbixSender']
+      py_modules=['ZabbixSender'],
+	  keywords='zabbix sender monitoring',
+	  classifiers=[
+			  'Development Status :: 2 - Pre-Alpha',
+			  'License :: OSI Approved :: Apache Software License',
+			  'Programming Language :: Python :: 3',
+			  'Topic :: System :: Monitoring',
+			  'Topic :: System :: Networking :: Monitoring',
+			  'Topic :: System :: Systems Administration'
+			  ]
       )
