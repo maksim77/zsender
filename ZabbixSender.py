a90c7a41ee90f5111cfbc975a329ab301db8d81a
Maksim Syomochkin <maksim77ster@gmail.com>
2016-02-28 17:33:20 +0300

    test commit for travis

--- ZabbixSender.py
+++ ZabbixSender.py
@@ -15,7 +15,7 @@ class ZabbixSender:
         else:
             self.server = server
         self.port = port
-        self.status = ''
+		self.status = '1'
 
     def __str__(self):
         return json.dumps({'server': self.server,
